671a1c04362e928a6e0c9bc547a1823b9f647cd5
Villsource <anirut1800@gmail.com>
2023-04-12 08:11:20 +0700

    add database

---
+++ BlogService/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BlogService.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogService.Data
+{
+    public class AppDbContext : DbContext
+    {
+        DbSet<PostEntity>? posts { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        { 
+        }
+    }
+}
---
+++ BlogService/Entity/PostEntity.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlogService.Entity
+{
+    public class PostEntity
+    {
+        [Key]
+        public Guid ID { get; set; }
+
+        [Required]
+        public string PostTitle { get; set; } = string.Empty;
+    }
+}
