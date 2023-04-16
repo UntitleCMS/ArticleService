53573e2d870f801d542d6b31f19c821e3be40b66
Villsource <anirut1800@gmail.com>
2023-04-16 12:39:53 +0700

    add myservice

--- BlogService/Program.cs
+++ BlogService/Program.cs
@@ -1,4 +1,5 @@
 using BlogService.Data;
+using BlogService.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +10,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add MyServide
+builder.Services.AddMyService();
 
 // Get connection string 
 string? DB_CONNECTION_STRING
---
+++ BlogService/Services/MyServiceExtension.cs
@@ -0,0 +1,14 @@
+﻿using BlogService.Services.PostServices;
+using BlogService.Services.PostServices.Interfaces;
+
+namespace BlogService.Services
+{
+    public static class MyServiceExtension
+    {
+        public static IServiceCollection AddMyService(this IServiceCollection services)
+        {
+            services.AddScoped<IPostService, PostService>();
+            return services;
+        }
+    }
+}
