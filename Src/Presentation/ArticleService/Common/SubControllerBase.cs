fe55e8347f2f8acb8fd7c57579618091e3b96038
VillSource <anirut1800@gmail.com>
2023-09-20 00:54:38 +0700

    feature: ⚙️ add exeption message converter

--- Src/Presentation/ArticleService/Common/SubControllerBase.cs
+++ Src/Presentation/ArticleService/Common/SubControllerBase.cs
@@ -1,4 +1,6 @@
+using Application.Common.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.Extensions.Primitives;
 
 namespace ArticleService.Common;
@@ -16,5 +18,44 @@ public class SubControllerBase : ControllerBase
     }
     public SubControllerBase() : base()
     {
+    }
+
+    public override OkObjectResult Ok([ActionResultObjectValue] object? value)
+    {
+        if (value is IResponseWrapper responseWrapper)
+        {
+            if (responseWrapper.IsSuccess)
+            {
+                return base.Ok(value);
+            }
+            else
+            {
+                return base.Ok(new
+                {
+                    message = responseWrapper.Message,
+                    isSuccess = false,
+                    errors = GetExceptionMessages(responseWrapper.Error)
+                });
+            }
+        }
+        return base.Ok(value);
+    }
+
+    private string[]? GetExceptionMessages(Exception? e)
+    {
+        if (e is null)
+            return null;
+
+        if (e is AggregateException aggregateException)
+        {
+            return GetExceptionMessages(aggregateException);
+        }
+        return new[] { e.Message };
+    }
+
+    private string[]? GetExceptionMessages(AggregateException e)
+    {
+        var err = e.InnerException;
+        return GetExceptionMessages(err);
     }
 }
